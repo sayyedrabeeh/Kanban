0cab67345cef5fb8e7f99eddc66e15caf7b33ea6
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-05-21 21:18:15 +0530

    signup varibles

--- frontend/kanban/src/pages/signup.jsx
+++ frontend/kanban/src/pages/signup.jsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signup (){
-
+    const [username , setUsername ] = useState('')
+    const [email , setEmail ] = useState('')
+    const [password , setPassword ] = useState('')
+    const [confirmPassword , setConfirmPassword ] = useState('')
+    const [isLoading , setIsLoading ] = useState(false)
+    const [errorMsg , setErrorMsg ] = useState('')
+    const navigate = useNavigate()
+    
     return(
         <>
         </>
