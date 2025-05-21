b1405796ff6c7232f825d3e93b0a6cb3ad69a47c
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-05-21 21:13:08 +0530

    signup ui inistalize

---
+++ frontend/kanban/src/pages/signup.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Signup (){
+
+    return(
+        <>
+        </>
+    )
+
+}
+export default Signup
